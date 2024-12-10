cdef7ddc021013d920a5e4c54350b254a449f42d
Kirbo-opps <burtonjolintom@gmail.com>
2024-12-10 11:44:19 +0100

    Update Menu.h
    MEnu avec class pour les settings

--- Steam Purgator/Menu.h
+++ Steam Purgator/Menu.h
@@ -1,24 +1,39 @@
+#pragma once
+
 #ifndef MENU_H
 #define MENU_H
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <vector>
 #include <string>
 
+using namespace std;
+using namespace sf;
+
 class Menu {
 public:
-    Menu(float width, float height, const std::vector<std::string>& options);
-    void draw(sf::RenderWindow& window);
+    Menu(float width, float height, const vector<string>& options);
+    void draw(RenderWindow& window);
     void moveUp();
     void moveDown();
     int getSelectedIndex() const;
-
 private:
     void updateFlechePosition();
-    std::vector<sf::Text> menuItems;
-    sf::Font font;
+    vector<Text> menuItems;
+    Font font;
     size_t selectedIndex;
-    sf::Text fleche;
+    Text fleche;
 };
 
-#endif // MENU_H
+class Settings {
+public:
+    Settings();
+    void increaseVolume(); // Pour augmenter de 10
+    void decreaseVolume(); // Pour diminuer de 10        
+    int getVolume() const;
+    void setCustomKey(const string& action, Keyboard::Key key);
+private:
+    int volume; // Volume de 0 à 100
+};
+#endif //MENU.H
