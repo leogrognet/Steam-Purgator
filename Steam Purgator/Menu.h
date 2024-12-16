e932a615d046042887cafdf4ef598b9470faf0a2
Léo Grognet <lgrognet@gaming.tech>
2024-12-16 21:58:38 +0100

    change

--- Steam Purgator/Menu.h
+++ Steam Purgator/Menu.h
@@ -1,27 +1,58 @@
-#include <SFML/Graphics.hpp>
-#include <vector>
-#include <string>
+#pragma once
 
 #ifndef MENU_H
 #define MENU_H
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <map>
+#include <string>
+#include <sstream> // Pour convertir les noms des touches en chaînes
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
 class Menu {
 public:
-    Menu(float width, float height, const std::vector<std::string>& options);
-    void draw(sf::RenderWindow& window);
+    Menu(float width, float height, const vector<string>& options);
+
+    void draw(RenderWindow& window);
+
     void moveUp();
     void moveDown();
+
+    void handleMouseClick(Vector2i mousePos);
+
     int getSelectedIndex() const;
 
-
-
 private:
+
     void updateFlechePosition();
-    std::vector<sf::Text> menuItems;
-    sf::Font font;
+
+    vector<Text> menuItems;
+    Font font;
     size_t selectedIndex;
-    sf::Text fleche;
+    Text fleche;
 };
 
+class Settings {
+public:
+    Settings();
 
-#endif // !1
+    void setCustomKey(const string& action, Keyboard::Key key);
+    void redefineKeys(Settings& settings, RenderWindow& window, const Font& font, const Sprite& background);
+
+    void increaseVolume(); // Pour augmenter de 10
+    void decreaseVolume(); // Pour diminuer de 10
+    int getVolume() const;
+
+private:
+
+    int volume; // Volume de 0 à 100
+
+    map<string, Keyboard::Key> customKeys;
+
+};
+#endif // MENU_H
