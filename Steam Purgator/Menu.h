1668aeea338a22d2d453c63748fde6a760846574
Kirbo-opps <burtonjolintom@gmail.com>
2024-12-19 12:26:03 +0100

    Update Menu.h

--- Steam Purgator/Menu.h
+++ Steam Purgator/Menu.h
@@ -8,8 +8,8 @@
 #include <vector>
 #include <map>
 #include <string>
-#include <sstream> // Pour convertir les noms des touches en chaînes
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 using namespace sf;
@@ -17,20 +17,12 @@ using namespace sf;
 class Menu {
 public:
     Menu(float width, float height, const vector<string>& options);
-
     void draw(RenderWindow& window);
-
     void moveUp();
     void moveDown();
-
-    void handleMouseClick(Vector2i mousePos);
-
     int getSelectedIndex() const;
-
 private:
-
     void updateFlechePosition();
-
     vector<Text> menuItems;
     Font font;
     size_t selectedIndex;
@@ -40,19 +32,9 @@ private:
 class Settings {
 public:
     Settings();
-
     void setCustomKey(const string& action, Keyboard::Key key);
     void redefineKeys(Settings& settings, RenderWindow& window, const Font& font, const Sprite& background);
-
-    void increaseVolume(); // Pour augmenter de 10
-    void decreaseVolume(); // Pour diminuer de 10
-    int getVolume() const;
-
 private:
-
-    int volume; // Volume de 0 à 100
-
     map<string, Keyboard::Key> customKeys;
-
 };
-#endif // MENU_H
+#endif
